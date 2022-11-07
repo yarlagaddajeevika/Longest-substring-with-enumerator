1b2b0bd77e519a8afe4a2f03213674626c94f7b2
yarlagaddajeevika <47218938+yarlagaddajeevika@users.noreply.github.com>
2022-11-07 14:40:56 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        best = 0
+        front = -1
+        for i, c in enumerate(s):
+            
+            if c in seen and front < seen[c]:
+                front = seen[c]
+                
+            seen[c] = i
+            
+            if (i - front) > best:
+                print(i)
+                best = i - front
+            
+        return best
